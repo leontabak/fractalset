a103e3ad77600440a3ac8503b73a606513bd4ed0
Leon Hannah Tabak <l.tabak@ieee.org>
2021-12-07 17:23:05 -0600

    Add directions for students. Remove solution to problem (so that students can reconstruct solution themselves.)

--- src/main/java/com/eonsahead/fractalset/Complex.java
+++ src/main/java/com/eonsahead/fractalset/Complex.java
@@ -3,38 +3,60 @@ package com.eonsahead.fractalset;
 
 
 public class Complex {
-    private final double real;
-    private final double imag;
+    /*
+     TO-DO: Define a class that models
+     complex numbers.
     
-    public Complex( double real, double imag ) {
-        this.real = real;
-        this.imag = imag;
-    } // Complex()
+     The class will have:
+       instance variables
+       a single constructor
+       a method named add() for adding the complex number to
+         another complex number 
+       a method named multiply() for multiplying the
+         complex number times another complex number
+       a method named magnitudeSquared() for computing
+         the square of the complex number's magnitude
     
-    public Complex add( Complex z ) {
-        double r = this.real + z.real;
-        double i = this.imag + z.imag;
-        return new Complex( r, i );
-    } // add( Complex )
+     Here is the mathematics that you need to know:
     
-    public Complex multiply( Complex z ) {
-        // u = (a + bi)
-        // v = (c + di)
-        // u * v = (a + bi)(c + di)
-        //       = ac + bi * di + adi + cbi
-        //       = (ac - bd) + (ad + cb)i
-        
-        double r = this.real * z.real - this.imag * z.imag;
-        double i = this.real * z.imag + this.imag * z.real;
-        
-        return new Complex( r, i);
-    } // multiply( Complex )
+       Let i be the square root of -1.
+       What follows is mathematical notation.
+       In your program there will be no variable
+       named i and no variable that holds the square
+       root of -1!
+       The instance variables of the Complex class
+       will both hold floating point values and
+       will both have the type double.
     
-    public double magnitudeSquared() {
-        double rSquared = this.real * this.real;
-        double iSquared = this.imag * this.imag;
-        
-        return rSquared + iSquared;
-    } // magnitudeSquared()
+       Let u be a complex number with a real part
+       equal to a and an imaginary part equal to b.
+       Both a and b are floating point values.
+       u = (a + bi)
+    
+       Let v be a complex number with a real part
+       equal to c and an imaginary part equal to d.
+       Both c and d are floating point values.
+       v = (c + di)
+    
+       Then the sum of the two complex numbers is:
+    
+       u + v = (a + c) + (b + d)i
+    
+       The sum is a complex number whose real part
+       is (a + c) and whose imaginary part is (b + d).
+    
+       And the product of the two complex numbers is:
+    
+       u * v = (a + bi)(c + di)
+             = ac + bi * di + adi + cbi
+             = (ac - bd) + (ad + cb)i
+    
+       The product is a complex number whose real part
+       is (ac - bd) and whose imaginary part is (ad + cb).
+    
+       The square of the magnitude of the complex number
+       u = a + bi is a * a + b * b.
+    */
+    
     
 } // Complex
